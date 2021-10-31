8f8d1dd195b8c4de55030eafce39afa54ca348eb
rgrosser-08 <59972112+rgrosser-08@users.noreply.github.com>
2021-10-31 12:55:59 -0400

    Added buttons and second activity

--- app/src/main/java/com/example/activitieschallenge/MainActivity.java
+++ app/src/main/java/com/example/activitieschallenge/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.activitieschallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.activitieschallenge.extra.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button1OnClick(View view) {
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        String message = "button1";
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+
+    public void button2OnClick(View view) {
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        String message = "button2";
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+
+    public void button3OnClick(View view) {
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        String message = "button3";
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/activitieschallenge/ScrollingActivity.java
@@ -0,0 +1,53 @@
+package com.example.activitieschallenge;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.activitieschallenge.databinding.ActivityScrollingBinding;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private ActivityScrollingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView_scrollingactivity);
+        if(message.equals("button2")){
+            textView.setText(R.string.button2text);
+        }
+        if(message.equals("button3")){
+            textView.setText(getString(R.string.button3text));
+        }
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
